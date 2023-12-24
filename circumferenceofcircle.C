e1647e35f4fe7066955752d7161f6b14f4041e49
Nayana <127602515+harsinth@users.noreply.github.com>
2023-12-24 13:49:24 +0530

    Add files via upload

---
+++ circumferenceofcircle.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int r,c;
+    printf("enter the radius of circle");
+    scanf("%d",&r);
+    c=2*3.14*r;
+    printf("circumference of circle is %d",c);
+    return 0;
+}
